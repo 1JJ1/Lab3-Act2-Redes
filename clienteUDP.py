1d194259e9c6171273211779cdeeecf2c3cb9225
1JJ1 <69682351+1JJ1@users.noreply.github.com>
2022-04-07 21:23:38 -0500

    Commit de archivos

---
+++ clienteUDP.py
@@ -0,0 +1,113 @@
+from http import server
+import socket
+import sys
+import os
+import threading
+from datetime import datetime
+import time
+
+
+num_client = int(input('Cuantos clientes desea crear? '))
+while (num_client <= 0 and num_client>25 ):
+    num_client = int(input('Por favor ingresar un número válido: '))
+
+class Main:    
+    def __init__(self):
+        self.lock = threading.Lock()
+    def funct(self, nombre):
+        self.lock.acquire()
+        log = open("./logs/"+nombre+' '+datetime.today().strftime('%Y-%m-%d-%H-%M-%S')+"log.txt", "w")
+        self.lock.release()
+
+        # Crear a UDP
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        
+        # Conectar socket al puerto donde se esta escuchando. IP de maquina virtual Ubuntu.
+        #server_addr = ('192.168.37.133', 8888)
+        server_addr = ('localhost', 8888)
+        print( 'connecting to %s port %s' % server_addr)
+        #sock.bind(server_addr)
+        sock.connect(server_addr)
+        file = open("./ArchivosRecibidos/"+nombre+"-prueba-"+str(num_client)+".txt", "w")
+        
+        try:
+            self.lock.acquire()
+            mensaje = b'Iniciar conexion...'
+            print ( 'enviando "%s"' % mensaje)
+            sock.sendto(mensaje,server_addr)
+        
+            confirmacion,server_addr = sock.recvfrom(32)
+            #print(confirmacion.decode('utf-8'))
+            # md5 = hashlib.md5()
+            if(confirmacion.decode('utf-8') == "ok"):
+                print('servidor mando ok')
+                sock.sendto(b'Cual es el nombre del archivo?',server_addr)
+                nA,server_addr = sock.recvfrom(32)
+                nombreArchivo = nA.decode('utf-8') 
+                log.write('El nombre es: '+nombreArchivo+'\n')
+                sock.sendto(b'listo',server_addr)
+                #self.lock.acquire()
+                #hash = sock.recv(32)
+                #print('Hash enviado por el servidor TCP:',hash.decode('utf-8'))
+                #sock.sendall(b'Hash recibido')
+                #self.lock.release()
+                num_paq=0
+                start = time.time()
+                err = False
+                while (True):      
+                    data,server_addr = sock.recvfrom(1024)
+                    
+                    if data:
+                        try:
+                            file.write(data.decode('utf-8') + os.linesep)
+                            #md5.update(data)
+                            num_paq+=1
+                            
+                        except:
+                            err = True
+                            print('Ocurrio un error') 
+                            sock.sendto(b'Hubo un error al recibir el archivo',server_addr)
+                            break
+                        
+                    else:
+                        print ('Final de lectura del archivo')
+                        sock.sendto(b'Archivo recibido',server_addr)
+                        break
+                end = time.time()
+                tam = os.path.getsize("./archivosRecibidos/"+nombre+"-prueba-"+str(num_client)+".txt")
+                
+
+                file.close()
+                log.write('El nombre del cliente es: '+nombre+'\n')
+                log.write('El tamaño del archivo es: '+str(tam/1000000)+' MB'+'\n')
+                
+                # print("Hash del archivo leido: {0}".format(md5.hexdigest()))
+                if err == False:
+                    print("Archivo leido")
+                    log.write('Entrega del archivo'+'\n')
+                else:
+                    log.write('Error entrega archivo'+'\n')
+
+                log.write('Tiempo de transferencia: '+str(end-start)+ ' segs'+'\n') 
+                log.write('Valor total en bytes recibidos: '+str(tam)+'\n')     
+                log.write('Cantidad de paquetes recibidos: '+str(num_paq)+'\n') 
+                         
+        finally:
+            
+            print ('Cerrar socket')
+            
+            self.lock.release() 
+            print ('Fin del programa')
+            log.close()
+        sock.close()
+           
+def targ(c, nombre):
+        c.funct(nombre)
+        
+hilo=Main()
+for num_cliente in range(num_client):
+    cliente = threading.Thread(name="Cliente%s" %(num_cliente+1),
+                               target=targ,
+                               args=(hilo,"Cliente%s" %(num_cliente+1))
+                              )
+    cliente.start()
---
+++ serverUDP.py
@@ -0,0 +1,95 @@
+import socket
+from sqlite3 import connect
+import sys
+import os 
+import hashlib
+import time
+from datetime import datetime
+
+from django.db import connection
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+num_conn = int(input('Bienvenido al servidor UDP. Por favor ingresar la cantidad de conexiones: '))
+
+while (num_conn <= 0 and num_conn>25):
+    num_conn = int(input('Por favor ingresar un número válido: '))
+
+
+# Conectar socket al puerto con su IP respectiva.
+# server_address = ('192.168.37.133', 8888)
+server_address = ('localhost', 8888)
+print('El %s esta esparando en el puerto %s' % server_address)
+sock.bind(server_address)
+
+#Archivo a enviar
+file = input('Por favor ingrese el nombre del archivo a enviar. ej: Prueba_100MB.txt o Prueba_250MB.txt ')
+while file not in ['Prueba_100MB.txt','Prueba_250MB.txt']:
+    file = input('Por favor ingresar un nombre de archivo correcto: ')
+
+archivoSize = os.path.getsize(file)
+
+log = open("./logs/servidor-"+' '+datetime.today().strftime('%Y-%m-%d-%H-%M-%S')+"log.txt", "w")
+log.write('Fecha: '+datetime.today().strftime('%Y-%m-%d-%H-%M-%S')+'\n')  
+log.write('El nombre es: '+file+'\n') 
+log.write('El tamaño del archivo es: '+str(archivoSize/1000000)+' MB'+'\n')
+#TOdo: logs faltantes 
+
+archivo = open(file, 'rb')
+buf = archivo.read(1024) #Definimos el tamaño de lectura en 1024 Bytes.
+
+while(buf):
+    buf = archivo.read(1024)
+ 
+# sock.listen(25)
+
+for i in range(num_conn):
+    
+    f = open(file,'rb')
+    l = f.read(1024)
+  
+    print ('El servidor esta a la espera de una conexión')
+    #connection, client_addr = sock.accept()
+    # Esperando conexion. Deberia llegar iniciar conexion
+    connection,client_addr = sock.recvfrom(32)
+    print(connection.decode('utf-8'))
+    start = time.time()
+    log.write('Direccion del cliente: '+str(client_addr)+'\n')  
+    try:
+        print ('Conectado de', client_addr)
+        
+        #data,client_addr = sock.recv(32)    
+        sock.sendto(b'ok',client_addr)
+        data,client_addr= sock.recvfrom(32)
+        print(data.decode('utf-8'))
+        sock.sendto(bytes(file, 'utf-8'),client_addr)
+        data,client_addr = sock.recvfrom(32)
+
+        print(data.decode('utf-8'))
+        if(data.decode('utf-8')== "listo"):
+            
+            # connection.sendall(bytes(md5.hexdigest(), 'utf-8'))
+            #recibido = connection.recv(32)
+            #if(recibido.decode('utf-8') == 'Hash recibido'):
+            while (l):
+                sock.sendto(l,client_addr) 
+                l= f.read(1024)
+            
+            sock.sendto(l,client_addr) 
+            print('Conexión terminada exitosamente')
+            log.write('Entrega del archivo: se envio el archivo '+'\n')  
+            end = time.time()
+            log.write('Tiempo transferencia con cliente: '+str(end-start)+'\n')  
+        data,client_addr = sock.recvfrom(32)
+            
+            
+    finally:
+        print('termino')
+
+log.close()
+sock.close()
+
+            
+
